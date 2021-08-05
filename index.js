c9270483600f7ddf629e481b925c60027892820c
Christian Hamburger Grøngaard <christianhg@gmail.com>
2021-08-05 22:15:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import {
+  getScale,
+  symbols,
+} from "./node_modules/semitone/dist/semitone.module.js";
+
+const majorScales = symbols.naturalNotes
+  .map((naturalNote) => [
+    getScale(`${naturalNote}${symbols.flat}`, "major"),
+    getScale(naturalNote, "major"),
+    getScale(`${naturalNote}${symbols.sharp}`, "major"),
+  ])
+  .flat()
+  .map((scale) => scale.join(" – "));
+
+const majorScalesList = document.getElementById("majorScales");
+
+majorScales.forEach((scale) => {
+  const li = document.createElement("li");
+  li.textContent = scale;
+  majorScalesList.appendChild(li);
+});
